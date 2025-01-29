fc89a77f3ca813e10e0ee833bf41006a833f89f4
Daniel-OBatista <daniel.celapp@gmail.com>
2025-01-29 12:54:30 -0300

    Comit

--- Flask/app.py
+++ Flask/app.py
@@ -2,13 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_word():
-    return "Hello Word!"
-
-@app.route("/about")
-def about():
-    return "Sobre mim..."
+#Crud  - Create, Reade, Update, Delete
 
 if __name__ == "__main__":
     app.run(debug=True)
